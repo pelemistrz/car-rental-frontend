f667d5b93e9b7e0f65c5602657768521c3806c5a
Marcin Pająk <39952424+pelemistrz@users.noreply.github.com>
2025-01-07 15:36:09 +0100

    Database schema

--- db.sql
+++ db.sql
@@ -1,6 +1,64 @@
-CREATE USER 'ecommerceapp'@'localhost' IDENTIFIED BY 'ecommerceapp';
 
-GRANT ALL PRIVILEGES ON * . * TO 'ecommerceapp'@'localhost';
+DROP SCHEMA IF EXISTS `car-rental`;
+
+CREATE SCHEMA `car-rental`;
+USE `car-rental` ;
+
+DROP TABLE IF exists `cars`;
+DROP TABLE IF exists `reservations`;
+DROP TABLE IF exists `equipment`;
+DROP TABLE IF exists `fuel_usage`;
+DROP TABLE IF exists `fuel_category`;
 
 
-ALTER USER 'carrental'@'localhost' IDENTIFIED WITH mysql_native_password BY 'carrental';
+CREATE TABLE `cars`(
+    `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `brand` VARCHAR(255) NOT NULL,
+    `model` VARCHAR(255) NOT NULL,
+    `daily_fee` DECIMAL(8, 2) NOT NULL,
+    `vehicle_status` VARCHAR(255) NOT NULL,
+    `date_created` DATETIME NOT NULL,
+    `last_updated` DATETIME NOT NULL,
+    `registration` VARCHAR(255) NOT NULL,
+    `daily_penalty` DECIMAL(8, 2) NOT NULL,
+    `fuel_consumption` DECIMAL(8, 2) NOT NULL,
+    `type_of_fuel` VARCHAR(255) NOT NULL
+);
+CREATE TABLE `reservations`(
+    `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `car_id` BIGINT NOT NULL,
+    `rental_date` DATE NOT NULL,
+    `return_date` DATE NOT NULL,
+    `equipment_id` BIGINT NOT NULL,
+    `real_return_date` DATE NOT NULL,
+    `total_fee` DECIMAL(8, 2) NOT NULL,
+    `penalty` DECIMAL(8, 2) NOT NULL,
+    `fuel_usage_id` BIGINT NOT NULL
+);
+CREATE TABLE `equipment`(
+    `id` BIGINT NOT NULL,
+    `description` VARCHAR(255) NOT NULL,
+    PRIMARY KEY(`id`)
+);
+CREATE TABLE `fuel_usage`(
+    `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `reservation_id` BIGINT NOT NULL,
+    `counter_reading_begin` BIGINT NOT NULL,
+    `counter_reading_end` BIGINT NOT NULL,
+    `fuel_usage` DECIMAL(8, 2) NOT NULL,
+    `cost_of_fuel` DECIMAL(8, 2) NOT NULL,
+    `fuel_category_id` BIGINT NOT NULL
+);
+CREATE TABLE `fuel_category`(
+    `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `description` VARCHAR(255) NOT NULL,
+    `cost_per_unit` DECIMAL(8, 2) NOT NULL
+);
+ALTER TABLE
+    `reservations` ADD CONSTRAINT `reservations_fuel_usage_id_foreign` FOREIGN KEY(`fuel_usage_id`) REFERENCES `fuel_usage`(`id`);
+ALTER TABLE
+    `reservations` ADD CONSTRAINT `reservations_car_id_foreign` FOREIGN KEY(`car_id`) REFERENCES `cars`(`id`);
+ALTER TABLE
+    `fuel_usage` ADD CONSTRAINT `fuel_usage_fuel_category_id_foreign` FOREIGN KEY(`fuel_category_id`) REFERENCES `fuel_category`(`id`);
+ALTER TABLE
+    `reservations` ADD CONSTRAINT `reservations_equipment_id_foreign` FOREIGN KEY(`equipment_id`) REFERENCES `equipment`(`id`);
