0b5f7e3a02be294a3a4b385b5f260571edf2756b
Marcin Pająk <39952424+pelemistrz@users.noreply.github.com>
2025-01-22 09:44:03 +0100

    Reserve a car - customer

--- src/app/components/rent-car/rent-car.component.ts
+++ src/app/components/rent-car/rent-car.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CarService } from '../../services/car.service';
 import { Car } from '../../common/car';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { RentValidators } from '../../validators/rent-validator';
 
 @Component({
   selector: 'app-rent-car',
@@ -14,11 +21,37 @@ export class RentCarComponent {
   car: Car = new Car();
   isLoaded: boolean = false;
 
-  constructor(private route: ActivatedRoute, private carService: CarService) {}
+  rentFormGroup: FormGroup;
+
+  constructor(
+    private route: ActivatedRoute,
+    private carService: CarService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(() => {
       this.handleCarDetails();
+    });
+
+    this.rentFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: new FormControl('', [
+          Validators.required,
+          Validators.minLength(2),
+          RentValidators.notOnlyWhitespace,
+        ]),
+        lastName: new FormControl('', [
+          Validators.required,
+          Validators.minLength(2),
+          RentValidators.notOnlyWhitespace,
+        ]),
+        email: new FormControl('', [
+          Validators.required,
+          Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+        ]),
+        country: new FormControl(''),
+      }),
     });
   }
 
@@ -29,5 +62,21 @@ export class RentCarComponent {
       this.car = data;
       this.isLoaded = true;
     });
+  }
+
+  onSubmit() {}
+
+  // gettery
+  get firstName() {
+    return this.rentFormGroup.get('customer.firstName');
+  }
+  get lastName() {
+    return this.rentFormGroup.get('customer.lastName');
+  }
+  get email() {
+    return this.rentFormGroup.get('customer.email');
+  }
+  get country() {
+    return this.rentFormGroup.get('customer.country');
   }
 }
